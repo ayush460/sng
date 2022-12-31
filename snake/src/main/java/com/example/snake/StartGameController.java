740fa59d1477e55c70c101f16780cd5b17980fe6
ayush460 <90319070+ayush460@users.noreply.github.com>
2022-12-31 21:30:13 +0530

    Add files via upload

---
+++ snake/src/main/java/com/example/snake/StartGameController.java
@@ -0,0 +1,13 @@
+package com.example.snake;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseButton;
+
+import java.io.IOException;
+
+public class StartGameController {
+    @FXML
+    public void start(MouseButton event) throws IOException{
+
+    }
+}
